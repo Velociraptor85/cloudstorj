44d5fbb7e2933b67192ccd7d51dae4d90b3f4570
idocoke <tfy12ery@student.lu.se>
2018-02-20 19:29:50 +0100

    Initial commit

---
+++ init_gcloud.sh
@@ -0,0 +1,4 @@
+#Install gcloud, have to click 'enter', then 'n', then 'y'
+curl https://sdk.cloud.google.com | bash ### Hit 'enter', 'no' to improve SDK, 'no' to continue
+exec -l $SHELL
+gcloud init ### Log in to your account, select project and remember the project name!, no default zone
