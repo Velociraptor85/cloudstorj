19a131bffab2dcc5b8eb9075f451e891bcd6754d
Velociraptor85 <Velociraptor85@users.noreply.github.com>
2018-02-27 23:46:01 +0100

    split
    splitting up the tasks and auto Yes apt-get, also git cloning the storjshare-daemon is not necessary because it is installed via npm

--- create_storj.sh
+++ create_storj.sh
@@ -3,6 +3,17 @@
 ### You will get some options, just enter the logical choice (most often Y)
 ### In the very end, you will arrive at a text editor: Write :wq and press enter
 
-#Install prereqs, get storjshare-daemon, install, initiate.
-gcloud compute ssh storj --command='sudo apt-get update && sudo apt-get install git python build-essential && git clone https://github.com/Storj/storjshare-daemon && wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash && export NVM_DIR="/home/erik/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install --lts && npm install --global storjshare-daemon && storjshare daemon && sudo mkdir /home/storj && sudo chmod 777 /home/storj && storjshare-create --storj '$2' --storage=/home/storj --size=500GB --rpcport=7000 --rpcaddress='$1' --manualforwarding -o ~/config && storjshare start --config ~/config'
+#Install prereqs
+gcloud compute ssh storj --command='sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install git python build-essential'
+
+#get nvm, npm, storjshare-daemon
+gcloud compute ssh storj --command='wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash '
+gcloud compute ssh storj --command='nvm install --lts && npm install --global storjshare-daemon'
+
+#prepare storage location 
+gcloud compute ssh storj --command='sudo mkdir /home/storj && sudo chmod 777 /home/storj'
+
+#start storj
+gcloud compute ssh storj --command='storjshare daemon && storjshare-create --storj '$2' --storage=/home/storj --size=500GB --rpcport=7000 --rpcaddress='$1' --manualforwarding -o ~/config && storjshare start --config ~/config'
+
+#maybe add watchdog script also
